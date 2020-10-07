03e81f01f83a602b7aac4e1673b020e6c9ebf047
Dawid Kawiak <d.kawiak@selleo.com>
2020-10-07 16:58:58 +0200

    Add styled components ThemeProvider

--- client/src/containers/Layout/Navbar.js
+++ client/src/containers/Layout/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ className }) => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/crud">Crud</Link></li>
-        <li><Link to="/d3">D3</Link></li>
+      <ul className={className}>
+        <li className="navigation-list__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navigation-list__item">
+          <Link to="/crud">Crud</Link>
+        </li>
+        <li className="navigation-list__item">
+          <Link to="/d3">D3</Link>
+        </li>
       </ul>
-    </nav>
   )
 }
 
--- client/src/containers/Layout/index.js
+++ client/src/containers/Layout/index.js
@@ -1,13 +1,65 @@
 import React from 'react'
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
 
 import Navbar from './Navbar'
 
+const theme = {
+  black: '#393939',
+  grey: '#3a3a3a',
+  white: '#ededed',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0,0,0, 0.09)',
+  bgDark: '#151935',
+  bgLight: '#1b223e',
+  accentColor: '#ffe400'
+}
+
+const InnerWrapper = styled.main`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    font-weight: 300;
+    line-height: 2;
+    font-family: 'Source Sans Pro';
+    background: ${theme.bgDark};
+    color: ${theme.white};
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`
+
+const StyledNavbar = styled(Navbar)`
+  display: flex;
+`
+
 const Layout = ({ children }) => {
   return (
-    <main>
-      <Navbar />
-      {children}
-    </main>
+    <ThemeProvider theme={theme}>
+      <StyledNavbar />
+      <InnerWrapper>
+        {children}
+      </InnerWrapper>
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
 
